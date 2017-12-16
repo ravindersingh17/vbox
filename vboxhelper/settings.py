f3ecd6febb805c8d2fab616456f4d15f9deaa1da
ravindersingh17 <n30,2006@gmail.com>
2017-12-16 19:16:29 +0530

    Bug fix

--- vboxhelper/settings.py
+++ vboxhelper/settings.py
@@ -2,14 +2,14 @@ import json, os
 
 class settings:
 
-    def __init__():
+    def __init__(self):
         homeDir = os.path.expanduser("~")
         self.dataFile = os.path.join(homeDir, ".vbox", "data.json")
         self.data = json.loads(open(dataFile).read())
 
-    def getAll():
+    def getAll(self):
         return self.data
 
-    def writeToDisk():
+    def writeToDisk(self):
         fhandle = open(self.dataFile, 'w')
         fhandle.write(json.dumps(data))
